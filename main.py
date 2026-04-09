034bfe4eaf40a3782dce8ba3160eb192a885b41a
Alejandro <AlejandroDZ2025@gmail.com>
2026-04-09 10:32:59 -0400

    new changes to main.py

--- main.py
+++ main.py
@@ -3,7 +3,27 @@ import os
 
 load_dotenv()
 
-clientid = os.getenv("spotify_clientid")
-rate = float(os.getenv("royalty_per_stream"))
-threshold = float(os.getenv("monetization_threshold"))
-label_commission = float(os.getenv("label_commission"))
+def main():
+    print("Welcome to the Spotify Market Value Ticker!")
+
+    # include audio and data processing here
+
+    # record 10 seconds of audio (include code here)
+    print("Play song now...")
+
+    # process and analyze the audio to identify the song
+    # include test track to see if API works correctly
+    song_title = "Test Song"
+    artist_name = "Test Artist"
+
+    # run the valuation
+    print(f"Generating market value for: {song_title} by {artist_name}")
+    reported_valuation = 1000000 #placeholder
+
+    #displaty the market dashboard
+    print("----------")
+    print(f"Track: {song_title} | Artist: {artist_name}")
+    print(f"Estimated Market Value: ${reported_valuation:.2f}")
+
+if __name__ == "__main__":
+    main()
