d987b6302838d8c47e1a85e92eb87dfdba9be79f
Alejandro <AlejandroDZ2025@gmail.com>
2026-05-03 08:58:02 -0400

    import new code into main

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
-# add pygame UI 
+import pygame 
+import requests
+from io import BytesIO
+from database import Database
+from music_data import MusicData
+
+pygame.init()
+pygame.mixer.init()
+
+screen = pygame.display.set_mode((400,300))
+pygame.display.set_caption("Streamfolio")
+title_font = pygame.font.SysFont("Arial", 24, bold = True)
+font_data = pygame.font.SysFont("Arial", 18)
+
+def load_image(url):
+    response = requests.get(url)
+    image = pygame.image.load(BytesIO(response.content))
+    return pygame.transform.scale(image, (200, 400))
+
+def audio_preview(url):
+    if url:
+        response = requests.get(url)
+        audio_file = BytesIO(requests.content)
+        pygame.mixer.music.load(audio_file)
+        pygame.mixer.music.play()
+    else:
+        pygame.mixer.music.stop()
+        print("No preview available for this track.")
+
+def main():
+
+
+
+if __name__ == "__main__":
+    main()
